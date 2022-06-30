b9a886b09772d9c9a7d69da9082ac9ebb2ce1076
Danzmd <107999522+Danzmd@users.noreply.github.com>
2022-06-30 12:04:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = '2022'
-global.author = '© Helga Zexs'
+global.author = '© DanzMd'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
