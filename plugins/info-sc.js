a33e936ca3776d76f3d9eaa907bb32076751c85d
Danzmd <107999522+Danzmd@users.noreply.github.com>
2022-07-05 17:44:45 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -16,7 +16,7 @@ Nih Link Sc Nya Dibawah :
 Pm ownernya kak kalo mau scnya
 
 _*DANZBOT*_`
-conn.sendBut(m.chat, esce, wm3, 'OWNER', 'OWNER', m) 
+conn.sendBut(m.chat, esce, wm3, 'Owner', 'owner', m) 
 }
 handler.help = ['sc', 'sourcecode']
 handler.tags = ['owner']
